b3ab438db3cb5c9e982a05ac23704531e83253cf
fsniraj <fsniraj@fsnirajs-MacBook-Pro.local>
2023-02-03 18:17:50 +0530

    understanding from ground

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import React from "react";
-import "./App.css";
-
-class SomeClass {}
-
-function App(props: { name: string }) {
+const App = () => {
   return (
-    <div
-      onClick={() => {
-        console.log("You just clicked!");
-      }}
-      className="App"
-    >
-      <h1>Hello world {props.name}</h1>
+    <div>
+      <h1>Hello world</h1>
     </div>
   );
-
-  // return React.createElement(
-  //   "div",
-  //   { className: "App" },
-  //   React.createElement("h1", null, "Hello world")
-  // );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const rootEl = document.getElementById("root") as HTMLElement;
+
+const root = ReactDOM.createRoot(rootEl);
+root.render(
   <React.StrictMode>
-    <App name="Anything" />
+    <App />
   </React.StrictMode>
 );
