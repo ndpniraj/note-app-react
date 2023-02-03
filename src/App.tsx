26bef50e1766d1d1118c46176e9490121f59ba18
fsniraj <fsniraj@fsnirajs-MacBook-Pro.local>
2023-02-03 18:31:38 +0530

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 const App = () => {
   return (
-    <div className="container">
-      <h1>Note Application</h1>
+    <div className="w-[500px] mx-auto bg-gray-200">
+      <h1 className="font-semibold text-2xl text-blue-400">Note Application</h1>
       <div>
-        <input placeholder="Title" type="text" className="input" />
+        <input placeholder="Title" type="text" className="" />
       </div>
       <div>
-        <textarea placeholder="Description" className="textarea"></textarea>
+        <textarea placeholder="Description" className=""></textarea>
       </div>
     </div>
   );
