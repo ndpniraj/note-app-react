7ca27121e9f13f94ca6979b65c627eaaf92a97e4
fsniraj <fsniraj@fsnirajs-MacBook-Pro.local>
2023-02-05 12:49:42 +0530

    react state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import NoteItem from "./components/NoteItem";
 
+// let title = "";
 const App = () => {
+  const [title, setTitle] = useState("hello");
+
   return (
     <div className="max-w-3xl mx-auto space-y-6">
       <div className="space-y-6 bg-white shadow-md rounded p-5">
         <h1 className="font-semibold text-2xl text-center">Note Application</h1>
         <div>
+          {title.trim() && title.length < 3 ? (
+            <p className="text-red-500">Title is too short</p>
+          ) : null}
           <input
             placeholder="Title"
             type="text"
             className="w-full border-b-2 border-gray-700 outline-none"
+            onChange={(evt) => {
+              setTitle(evt.target.value);
+            }}
+            value={title}
           />
         </div>
         <div>
@@ -20,7 +30,12 @@ const App = () => {
           ></textarea>
         </div>
         <div className="text-right">
-          <button className="bg-blue-500 text-white px-5 py-2 rounded">
+          <button
+            onClick={() => {
+              console.log(title);
+            }}
+            className="bg-blue-500 text-white px-5 py-2 rounded"
+          >
             Submit
           </button>
         </div>
