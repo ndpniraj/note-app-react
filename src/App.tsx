eedb75159bfdad6486a649e293ecde4d1f5cd41e
fsniraj <fsniraj@fsnirajs-MacBook-Pro.local>
2023-02-05 12:55:55 +0530

    updating state from single place

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, ChangeEventHandler } from "react";
 import NoteItem from "./components/NoteItem";
 
 // let title = "";
 const App = () => {
-  const [title, setTitle] = useState("hello");
+  // const [title, setTitle] = useState("");
+  // const [description, setDescription] = useState("");
+
+  const [values, setValues] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = ({ target }) => {
+    const { name, value } = target;
+    setValues({ ...values, [name]: value });
+  };
 
   return (
     <div className="max-w-3xl mx-auto space-y-6">
       <div className="space-y-6 bg-white shadow-md rounded p-5">
         <h1 className="font-semibold text-2xl text-center">Note Application</h1>
         <div>
-          {title.trim() && title.length < 3 ? (
-            <p className="text-red-500">Title is too short</p>
-          ) : null}
           <input
             placeholder="Title"
             type="text"
             className="w-full border-b-2 border-gray-700 outline-none"
-            onChange={(evt) => {
-              setTitle(evt.target.value);
-            }}
-            value={title}
+            onChange={handleChange}
+            value={values.title}
+            name="title"
           />
         </div>
         <div>
           <textarea
             placeholder="Description"
             className="w-full border-b-2 border-gray-700 outline-none resize-none h-36"
+            value={values.description}
+            onChange={handleChange}
+            name="description"
           ></textarea>
         </div>
         <div className="text-right">
           <button
             onClick={() => {
-              console.log(title);
+              console.log(values);
             }}
             className="bg-blue-500 text-white px-5 py-2 rounded"
           >
